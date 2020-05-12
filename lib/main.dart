e2f328f4a3141d45f4fbf237212a21cdaeaf3a38
Yuchan <mikuplayer123@gmail.com>
2020-05-13 04:34:30 +0900

    Change to Asset Image

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,12 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           title: Text('I Am Misuzu'),
-          backgroundColor: Colors.blueGrey[900],
+          backgroundColor: Colors.black,
         ),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.pinkAccent,
         body: Center(
           child: Image(
-            image: NetworkImage('https://ww.namu.la/s/b93c2be4f43685db6885e9558967f7401d0d7c8fc75da32b1e1a1054b019e9fa032d926cd1c4799795448171c56cc906533075dc2f409d77379de45ade080786df68232cb760da618395f54e9bcb42ce74b9a9643ac886ad352c32fc942d1ee2'),
+            image: AssetImage('images/misuzu.png'),
           ),
         ),
       ),
